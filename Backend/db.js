f6ad0550e51da39578fbc871f99ce562bc28054d
Fardaan Mahdi <codingmonk501@gmail.com>
2024-03-03 02:25:46 +0530

    backend setup

---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+const mongoURI="mongodb://localhost:27017/"
+
+const connectToMongo=async()=>{
+    try{
+    mongoose.connect(mongoURI); 
+    console.log("mongo connected");
+    }catch(error){
+        console.log(error);
+        process.exit();
+    }
+}
+
+module.exports=connectToMongo;
